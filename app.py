dce18b104bca2243295b968bf791dc2ffbc4d6ad
Carl Molina <carlandrew.molina@gmail.com>
2024-01-30 16:30:16 -0800

    commented out secret key for later use

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ import boto3
 AWS_ACCESS_KEY = os.environ['aws_access_key_id']
 AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
 BUCKET_NAME = os.environ['bucket_name']
-DATABASE_URL = os.environ['database_url']
+# DATABASE_URL = os.environ['database_url']
+REGION_CODE = os.environ['region_code']
+# SECRET_KEY = os.environ['secret_key']
+
+
+# Virtual-hosted–style requests
+# https://<bucket-name>.s3.<region-code>.amazonaws.com/<key-name>
 
 from flask import Flask, request, render_template, flash
 from flask_uploads import UploadSet, configure_uploads, IMAGES
@@ -16,12 +22,14 @@ from flask_uploads import UploadSet, configure_uploads, IMAGES
 # from werkzeug.utils import secure_filename
 # from werkzeug.datastructures import FileStorage
 
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
+
 # from sqlalchemy.exc import IntegrityError <-- db, not needed right now
 
 # import exifread <-- how to read exif data from photo file
 
 app = Flask(__name__)
+# debug = DebugToolbarExtension(app)
 
 # app.config['S3_BUCKET'] = 'S3_BUCKET_NAME'
 # app.config['S3_KEY'] = 'AWS_ACCESS_KEY'
@@ -48,24 +56,39 @@ s3 = boto3.client(
 def homepage():
     """Show homepage"""
 
-    return render_template('base.html')
+    # attempt to retrieve photo(s) from S3 to put on homepage
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # checks for file type
-        if 'file' not in request.files:
-            return 'No file part'
 
-        file = request.files['file']
 
-        if file.filename == '':
-            return 'No selected file'
+    photo_url = f'https://{BUCKET_NAME}.s3.{REGION_CODE}.amazonaws.com/chowder.jpeg'
 
-        # if passes all checks, uploads file to S3 bucket
-        s3.upload_fileobj(file, 'name-of-bucket', file.filename)
+    return render_template('base.html', photo_url=photo_url)
 
-        flash("File uploaded successfully")
 
-    flash("Method not allowed. Failed to upload file.")
+
+
+
+
+
+
+
+
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     if request.method == 'POST':
+#         # checks for file type
+#         if 'file' not in request.files:
+#             return 'No file part'
+
+#         file = request.files['file']
+
+#         if file.filename == '':
+#             return 'No selected file'
+
+#         # if passes all checks, uploads file to S3 bucket
+#         s3.upload_fileobj(file, 'name-of-bucket', file.filename)
+
+#         flash("File uploaded successfully")
+
+#     flash("Method not allowed. Failed to upload file.")
