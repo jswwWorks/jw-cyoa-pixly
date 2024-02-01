cfda1e90011c3fd423ee56efe633c35c3496030e
Carl Molina <carlandrew.molina@gmail.com>
2024-01-31 20:56:47 -0800

    reduced comments and non-used code

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 import os
-# from dotenv import load_dotenv
-# import requests
+from dotenv import load_dotenv
+import requests
 
 from s3_helpers import upload_to_s3
 
 # load environment variables from .env file
-# load_dotenv()
+load_dotenv()
 
 import boto3
-# s3 = boto3.resource('s3')
 
 AWS_ACCESS_KEY = os.environ['aws_access_key_id']
 AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
 BUCKET_NAME = os.environ['bucket_name']
-# DATABASE_URL = os.environ['database_url']
 REGION_CODE = os.environ['region_code']
 
 from flask import Flask, request, render_template, flash, redirect
@@ -32,35 +30,13 @@ app.config['SECRET_KEY'] = os.environ['secret_key']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-
-print('database url: ', os.environ['DATABASE_URL'])
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 
 connect_db(app)
 
-# Virtual-hosted–style requests
-# https://<bucket-name>.s3.<region-code>.amazonaws.com/<key-name>
+from flask_debugtoolbar import DebugToolbarExtension
 
-
-# from werkzeug.utils import secure_filename
-# from werkzeug.datastructures import FileStorage
-
-# from flask_debugtoolbar import DebugToolbarExtension
-
-# import exifread <-- how to read exif data from photo file
-
-# debug = DebugToolbarExtension(app)
-
-# app.config['S3_BUCKET'] = 'S3_BUCKET_NAME'
-# app.config['S3_KEY'] = 'AWS_ACCESS_KEY'
-# app.config['S3_SECRET'] = 'AWS_ACCESS_SECRET'
-# app.config['S3_LOCATION'] = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
-
-photos = UploadSet("photos", IMAGES)
-
-
-app.config["UPLOADED_PHOTOS_DEST"] = "static/img"
-configure_uploads(app, photos)
+debug = DebugToolbarExtension(app)
 
 
 # attempt at configuring S3 connection, "manage via python" from lecture
@@ -80,27 +56,17 @@ def homepage():
 
     # Source: https://stackoverflow.com/questions/44238525/how-to-iterate-over-files-in-an-s3-bucket
     paginator = s3.get_paginator('list_objects_v2')
-    # print(paginator)
     page_iterator = paginator.paginate(Bucket=BUCKET_NAME)
-    # print("page iterator", page_iterator)
 
 
     photos_urls = []
 
     for page in page_iterator:
-        # print("accessed a page")
         if page['KeyCount'] > 0:
             for file in page['Contents']:
-                # print(file, "this is the file")
-                # print(type(file))
-                # print("this is the file name:", file["Key"])
                 filename = file["Key"]
-                # filename = file.filename
                 photo_url = f'https://{BUCKET_NAME}.s3.{REGION_CODE}.amazonaws.com/{filename}'
                 photos_urls.append(photo_url)
-
-
-    # photo_url = f'https://{BUCKET_NAME}.s3.{REGION_CODE}.amazonaws.com/risotto.jpeg'
 
     return render_template('base.html', photos_urls=photos_urls)
 
@@ -108,40 +74,15 @@ def homepage():
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_photo():
     if request.method == 'POST':
-        # checks for file type
         file = request.files['photo']
-
-        # print('This is file: ', file)
         # after receiving valid photo file from form, we get a 'FileStorage'
         # object w/ methods and properties on it, most importantly, 'filename'.
         if file:
-
-
-# TODO: scaffolding code for extracting exif data from photos:
-# attribution for exifread: https://pypi.org/project/ExifRead/
-
-            # open image file for reading (must be in binary mode)
-            # TODO: need exact path name of file being uploaded
-
-            # file_path = os.path.abspath(file)
-            # print('This is file_path: ', file_path)
-
-            # photo_file = open(file_path, 'rb')
-
-            # return exif tags
-            # print('Before tags')
-            # TODO: bookmark
             tags = exifread.process_file(file)
-            # copy_of_tags = tags.deepcopy()
-            # print('After tags: ', tags)
+            for tag in tags.keys():
+                if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
+                    print("Key: %s, value %s" % (tag, tags[tag]))
 
-            # for tag in tags.keys():
-            #     if tag not in ('JPEGThumbnail', 'TIFFThumbnail', 'Filename', 'EXIF MakerNote'):
-            #         print("Key: %s, value %s" % (tag, tags[tag]))
-
-
-
-# TODO: bookmark
             metadata_tags = {}
             filename = file.filename
             metadata_tags["filename"] = filename
@@ -149,37 +90,10 @@ def upload_photo():
             for key, value in tags.items():
                 if key not in metadata_tags and key in photos_metadata_colname_conversions:
                     conversion = photos_metadata_colname_conversions[key]
-                    print('This is conversion: ', conversion)
-
-
-
-            #         # metadata_tags[] =
-
                     metadata_tags[conversion] = str(value)
 
-            #         # if conversion in numeric_cols: # converts to # in rare case
-            #         #     metadata_tags[conversion] = value
-
-            #         # print('metadata conversion for this entry', metadata_tags[conversion])
-
-
-
-            # print('metadata_tags before submit_photo: ', metadata_tags)
-
-
-            # print('This is metadata tags', metadata_tags)
             new_photo_in_db = Photo.submit_photo(metadata_tags)
             print('new_photo_in_db: ', new_photo_in_db)
-
-
-            # TODO: add filename to record in db
-
-            # if key in tags not in metadata_tags
-            # and also check that it's in photos_metadata_colname_conversions:
-            #     add key to metadata tags
-            #     but before that, convert it using photos_metadata_colname_conversions
-
-
 
             # puts cursor back to the beginning of the file
             file.seek(0)
@@ -187,38 +101,5 @@ def upload_photo():
             upload_to_s3(file, filename)
             # TODO: ^ this closes file
             flash('File uploaded successfully!')
-
-
-            # make in-memory copy of file to do separate operations on it
-
-
-
-
             return redirect('/')
     return render_template('form.html')
-
-
-
-
-
-
-
-
-# @app.route('/upload', methods=['GET', 'POST'])
-# def upload():
-#     if request.method == 'POST':
-#         # checks for file type
-#         if 'file' not in request.files:
-#             return 'No file part'
-
-#         file = request.files['file']
-
-#         if file.filename == '':
-#             return 'No selected file'
-
-#         # if passes all checks, uploads file to S3 bucket
-#         s3.upload_fileobj(file, 'name-of-bucket', file.filename)
-
-#         flash("File uploaded successfully")
-
-#     flash("Method not allowed. Failed to upload file.")
--- models.py
+++ models.py
@@ -1,230 +1,50 @@
-"""SQLAlchemy models for pixly app"""
-
-from datetime import datetime
-
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import IntegrityError
-
-db = SQLAlchemy()
-
-def connect_db(app):
-    """Connect this database to provided Flask app.
-
-    You should call this in your Flask app.
-    """
-
-    app.app_context().push()
-    db.app = app
-    db.init_app(app)
+import os
+from dotenv import load_dotenv
+import boto3
+from botocore.exceptions import ClientError
 
 
-photos_metadata_colname_conversions = {
-    "Image Make": "make",
-    "Image Model": "model",
-    "Image Orientation": "orientation_rotation",
-    "Image Software": "software",
-    "Image DateTime": "date_and_time",
-    "Image YCbCrPositioning": "ycbcr_positioning",
-    "Image XResolution": "x_resolution",
-    "Image YResolution": "y_resolution",
-    "Image ResolutionUnit": "resolution_unit",
-    "EXIF ExposureTime": "exposure_time",
-    "EXIF FNumber": "f_number",
-    "EXIF ExposureProgram": "exposure_program",
-    "EXIF ExifVersion": "exif_version",
-    "EXIF DateTimeOriginal": "date_and_time_original",
-    "EXIF DateTimeDigitized": "date_and_time_digitized",
-    "EXIF ComponentsConfiguration": "components_configuration",
-    "EXIF ExposureBiasValue": "exposure_bias",
-    "EXIF MeteringMode": "metering_mode",
-    "EXIF Flash": "flash",
-    "EXIF FocalLength": "focal_length",
-    "EXIF UserComment": "maker_note",
-    "EXIF FlashPixVersion": "flashpix_version",
-    "EXIF ColorSpace": "color_space",
-    "Interoperability InteroperabilityIndex": "interoperability_index",
-    "Interoperability InteroperabilityVersion": "interoperability_version"
-}
+# load environment variables from .env file
+load_dotenv()
 
-numeric_cols = ["x_resolution", "y_resolution", "exposure_bias"]
+AWS_ACCESS_KEY = os.environ['aws_access_key_id']
+AWS_SECRET_ACCESS_KEY = os.environ['aws_secret_access_key']
+BUCKET_NAME = os.environ['bucket_name']
+REGION_CODE = os.environ['region_code']
+SECRET_KEY = os.environ['secret_key']
+
+LOCAL_FILE = 'test_file.txt'
+NAME_FOR_S3 = 'test_file.txt'
+
+print('This is region code', REGION_CODE)
+print('This is secret key', SECRET_KEY)
+print('This is bucket name', BUCKET_NAME)
 
 
-class Photo(db.Model):
-    """Photo in the database."""
 
-    __tablename__ = "photos_metadata"
 
-    id = db.Column(
-        db.Integer,
-        primary_key=True,
+def upload_to_s3(file, filename):
+    print('In upload fn')
+
+    s3_client = boto3.client(
+        service_name='s3',
+        region_name=REGION_CODE,
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
     )
 
-    filename = db.Column(
-        db.String(150),
-        nullable=True,
-    )
+    print('This is bucket name', BUCKET_NAME)
 
-    make = db.Column(
-        db.String(60),
-        nullable=True,
-    )
+    print('This is file', file)
+    print('This is filename', filename)
 
-    model = db.Column(
-        db.String(30),
-        nullable=True,
-    )
+    resp = s3_client.upload_fileobj(file, BUCKET_NAME , filename)
 
-    orientation_rotation = db.Column(
-        db.String(30),
-        nullable=True,
-    )
+    print(f'upload file response: {resp}')
 
-    software = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    date_and_time = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    ycbcr_positioning = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    x_resolution = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    y_resolution = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    resolution_unit = db.Column(
-        db.String(25),
-        nullable=True,
-    )
-
-    exposure_time = db.Column(
-        db.String(15),
-        nullable=True,
-    )
-
-    f_number = db.Column(
-        db.String(10),
-        nullable=True,
-    )
-
-    exposure_program = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    exif_version = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    date_and_time_original = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    date_and_time_digitized = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    components_configuration = db.Column(
-        db.String(25),
-        nullable=True,
-    )
-
-    exposure_bias = db.Column(
-        db.String(30),
-        nullable=True,
-    )
-
-    metering_mode = db.Column(
-        db.String(20),
-        nullable=True,
-    )
-
-    flash = db.Column(
-        db.String(40),
-        nullable=True,
-    )
-
-    focal_length = db.Column(
-        db.String(10),
-        nullable=True,
-    )
-
-    maker_note = db.Column(
-        db.String(100),
-        nullable=True,
-    )
-
-    flashpix_version = db.Column(
-        db.String(35),
-        nullable=True,
-    )
-
-    color_space = db.Column(
-        db.String(20),
-        nullable=True,
-    )
-
-    interoperability_index = db.Column(
-        db.String(10),
-        nullable=True,
-    )
-
-    interoperability_version = db.Column(
-        db.String(20),
-        nullable=True,
-    )
-
-    @classmethod
-    def submit_photo(self, metadata_tags):
-
-        print('This is metadata_tags: ', metadata_tags)
-
-        # test print for keyword args passed into fn
-        # for key, value in args.items():
-        #     print("%s == %s" % (key, value))
+    # boto closes file
 
 
-        print('Before new photo')
-        try:
-            new_photo = Photo(**metadata_tags)
-            print('After new photo: ', new_photo)
-            print('new_photo.make: ', new_photo.make)
-        except IntegrityError:
-            print('error occurred', IntegrityError)
 
-
-        db.session.add(new_photo)
-
-        db.session.commit()
-
-        return new_photo
-
-
-        # Warbler code for reference:
-        # )
-
-        #     user = User(
-        #     username=username,
-        #     email=email,
-        #     password=hashed_pwd,
-        #     image_url=image_url,
-        # )
-
-        # db.session.add(user)
-        # return user
-
+def view_photos_from_s3():
+    print('In view_photos_from_s3')
